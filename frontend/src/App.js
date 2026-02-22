9c376750147ad295fca87cdf9ae91894d8442c74
Yodsakit <Yodsakit.123@gmail.com>
2026-02-22 23:14:44 +0700

    restructure all files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,19 +8,8 @@ import { CartProvider } from './contexts/CartContext';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import PrivateRoute from './components/PrivateRoute';
 
 import Home from './pages/Home';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import Orders from './pages/Orders';
-import OrderDetail from './pages/OrderDetail';
-import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
   return (
@@ -32,41 +21,6 @@ function App() {
             <main className="flex-grow">
               <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/products/:slug" element={<ProductDetail />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                
-                <Route path="/checkout" element={
-                  <PrivateRoute>
-                    <Checkout />
-                  </PrivateRoute>
-                } />
-                
-                <Route path="/profile" element={
-                  <PrivateRoute>
-                    <Profile />
-                  </PrivateRoute>
-                } />
-                
-                <Route path="/orders" element={
-                  <PrivateRoute>
-                    <Orders />
-                  </PrivateRoute>
-                } />
-                
-                <Route path="/orders/:orderNumber" element={
-                  <PrivateRoute>
-                    <OrderDetail />
-                  </PrivateRoute>
-                } />
-                
-                <Route path="/admin" element={
-                  <PrivateRoute roles={['ADMIN', 'MANAGER']}>
-                    <AdminDashboard />
-                  </PrivateRoute>
-                } />
               </Routes>
             </main>
             <Footer />
